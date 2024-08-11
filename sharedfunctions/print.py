ff303630a5496043ffbf0dd0138cf482b2d4088f
sillyjason <sillyjason.cao@gmail.com>
2024-08-11 22:29:15 +0800

    updated print colors

--- sharedfunctions/print.py
+++ sharedfunctions/print.py
@@ -6,5 +6,4 @@ def print_error(message):
     print(f"\033[1m\033[31m{message}\033[0m")
 
 def print_bold(message):
-    print(f"\033[1m\033[30m{message}\033[0m")
-    
+    print(f"\033[1m\033[94m{message}\033[0m")
